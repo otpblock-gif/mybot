56b6f4fe7e529fbef98e353293ab5eb1604c51c2
otpblock-gif <otpblock@gmail.com>
2025-10-05 10:53:49 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settings = {
+  packname: 'Lucky Tech Hub Bot',
+  author: '‎',
+  botName: "L   T   H  Bot",
+  botOwner: 'Lucky 218', // Your name
+  ownerNumber: '923268848703', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "👥public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "> ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ: ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ꜰᴏʀ ᴍᴀɴᴀɢɪɴɢ ɢʀᴏᴜᴘ ᴄᴏᴍᴍᴀɴᴅꜱ ᴀɴᴅ ᴀᴜᴛᴏᴍᴀᴛɪɴɢ ᴛᴀꜱᴋꜱ.",
+  caption: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴜᴄᴋʏ ᴛᴇᴄʜ ʜᴜʙ",
+  version: "2.2.6 alpha-8",
+  prefix: ".", // prefix (e.g., ., /, !, *)
+  timezone: "Africa/Kampala", //put your country timeZone....leave blank if u don't know
+  updateZipUrl: "https://www.mediafire.com/file/zlbozqwb014g68w/LuckyTechHub-Bot-main.zip/file",
+};
+
+module.exports = settings;
